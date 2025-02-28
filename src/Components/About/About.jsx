7fda25e59f6d41e8eca54ac5d50cc9e9b70cc652
clinton97 <clintonkipkoechngeno@gmail.com>
2025-02-28 23:18:47 +0300

    made change to th eabout page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -33,18 +33,15 @@ const About = () => {
         <div className="flex flex-col md:flex-row items-start space-y-8 md:space-y-0 md:space-x-12">
           <div className="md:w-2/3">
           <p className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>
-              I'm Clinton, a skilled front-end web developer freelancer. I specialize in creating user-friendly websites
-              and web apps that engage and convert users. Proficient in HTML, CSS, JavaScript, front-end frameworks-
-              React, Redux, and API integration. I bring designs to life and deliver seamless user experiences.
-            </p>
-            <p className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>
-              I have a keen interest in developing websites with smooth transitions and exceptional performance. I can
-              seamlessly join your team, using efficient continuous integration methods and tools like Git.
-            </p>
+          I'm Clinton, a software developer passionate about building modern, scalable web applications with seamless user experiences. Proficient in JavaScript, React, Redux, HTML, CSS, and Tailwind CSS, I specialize in crafting intuitive and high-performance interfaces.  
+          </p>
+          <p className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>              
+          I have a keen interest in smooth transitions, optimized performance, and API integration to enhance usability. With strong problem-solving skills and a collaborative mindset, I thrive in team-oriented environments, leveraging tools like Git and Agile methodologies for efficient development. My goal is to combine software development with data-driven insights to create impactful solutions.
+          </p>
             <p className="mb-4 text-base  text-teal-500 font-semibold">Here are a few technologies I've been working with recently:</p>
-            <ul className="grid grid-cols-2 gap-2 text-sm">
-              <li>HTML/CSS</li>
+            <ul className="grid grid-cols-2 gap-2 text-sm">              
               <li>JavaScript (ES6+)</li>
+              <li>HTML/CSS</li>
               <li>React</li>
               <li>Next.js</li>
               <li>TypeScript</li>
