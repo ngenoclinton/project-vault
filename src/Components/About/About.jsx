4963af2deed842b31ae2ebdecc1f8def0921ed22
clinton97 <clintonkipkoechngeno@gmail.com>
2025-03-01 23:24:15 +0300

    changed about page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -32,17 +32,23 @@ const About = () => {
 
         <div className="flex flex-col md:flex-row items-start space-y-8 md:space-y-0 md:space-x-12">
           <div className="md:w-2/3">
+          <ul className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>
           <p className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>
-          I'm Clinton, a software developer passionate about building modern, scalable web applications with seamless user experiences. Proficient in JavaScript, React, Next.js, HTML, CSS, and Tailwind CSS, I specialize in crafting intuitive and high-performance interfaces.  
+          I'm Clinton, a software developer passionate about building modern, scalable web applications with seamless user experiences.  
           </p>
-          <p className={`${  darkMode ? "text-Grey4" : "text-slate-800"} text-sm sm:text-base leading-relaxed mb-4`}>              
-          I have a keen interest in smooth transitions, optimized performance, and API integration to enhance usability. With strong problem-solving skills and a collaborative mindset, I thrive in team-oriented environments, leveraging tools like Git and Agile methodologies for efficient development. My goal is to combine software development with data-driven insights to create impactful solutions.
-          </p>
+          <li>Experience with React: Demonstrated experience in developing web applications using React.</li>
+          <li> Proficiency in Tailwind CSS: Strong understanding and practical experience with Tailwind CSS for creating responsive and aesthetically pleasing designs.</li>
+          <li>Knowledge of Web Technologies: Solid understanding of HTML, CSS, and JavaScript.</li>
+          <li>Version Control: Familiarity with Git and version control workflows.</li>
+          <li>Problem-Solving Skills: Strong analytical and problem-solving abilities.</li>
+          <li>Communication: Excellent verbal and written communication skills.</li>
+          <li>Team Player: Ability to work effectively in a remote team environment.</li>
+          </ul>
             <p className="mb-4 text-base  text-teal-500 font-semibold">Here are a few technologies I've been working with recently:</p>
             <ul className="grid grid-cols-2 gap-2 text-sm">              
-              <li>JavaScript (ES6+)</li>
+              <li>JavaScript</li>
+              <li>React</li>              
               <li>HTML/CSS</li>
-              <li>React</li>
               <li>Next.js</li>
               <li>TypeScript</li>
               <li>TailwindCSS</li>
